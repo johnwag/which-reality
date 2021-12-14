027120f725ae32e458e23f8c46f2ce11a70cc7f8
johnwag <johnwag@gmail.com>
2021-12-14 14:16:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <title>Oh geez... What reality am I in?</title>
 </head>
 
-<!-- TODO: Remember to upgrade Azure subscription before trial ends. [redacted]. -->
   
 <body>
 <h1> Server Information</h1>
